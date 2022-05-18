a5e5f81f8bcdbc8c82d478fc651aa1623e5a750a
rawwr <akshayace0302@gmail.com>
2022-05-18 20:06:23 +0530

    setters

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordian = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
   const onTitleClick = (index) => {
-    console.log("Title clicked:", index);
+    setActiveIndex(index);
   };
+
   const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={item.title}>
@@ -17,7 +20,12 @@ const Accordian = ({ items }) => {
       </React.Fragment>
     );
   });
-  return <div className="ui styled accordion">{renderedItems}</div>;
+  return (
+    <div className="ui styled accordion">
+      {renderedItems}
+      <h1>{activeIndex}</h1>
+    </div>
+  );
 };
 
 export default Accordian;
