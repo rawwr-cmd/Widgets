32f5dfd652d37dd021ab19a698b83f8f4def903b
rawwr <akshayace0302@gmail.com>
2022-05-19 11:26:48 +0530

    default search terms

---
+++ src/components/Search.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Search = () => {
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  console.log(results);
+
+  useEffect(() => {
+    const Search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      //   console.log(data);
+      setResults(data.query.search);
+    };
+
+    if (term) {
+      Search();
+    }
+  }, [term]);
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(event) => setTerm(event.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
