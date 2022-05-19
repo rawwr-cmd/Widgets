546b1539cacfe41a5adc3bad9e96fe25216e2d53
rawwr <akshayace0302@gmail.com>
2022-05-20 00:48:25 +0530

    binding an event handler

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    document.body.addEventListener(
+      "click",
+      () => {
+        console.log("Body clicked");
+        setOpen(false);
+      },
+      { capture: true }
+    );
+  }, []);
+
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
       return null;
@@ -10,7 +22,10 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
       <div
         key={option.value}
         className="item"
-        onClick={() => onSelectedChange(option)}
+        onClick={() => {
+          console.log("Item clicked");
+          onSelectedChange(option);
+        }}
       >
         {option.label}
       </div>
@@ -22,7 +37,10 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
       <div className="field">
         <label className="label">Select a Color</label>
         <div
-          onClick={() => setOpen(!open)}
+          onClick={() => {
+            console.log("DROPDOWN CLICKED");
+            setOpen(!open);
+          }}
           className={`ui selection dropdown ${open ? "visible active" : ""}`}
         >
           <i className="dropdown icon"></i>
