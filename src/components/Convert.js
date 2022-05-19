77588dd8531ad509c9c0c6d9fcd335d0ac4e1493
rawwr <akshayace0302@gmail.com>
2022-05-20 05:21:22 +0530

    displaying translated texts

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,10 +1,31 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState("");
   useEffect(() => {
-    console.log("New language or text");
+    const doTranslate = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    doTranslate();
   }, [language, text]);
-  return <div />;
+
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;
